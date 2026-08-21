c9740d0b822156328c4b1b50011abb4283f26a96
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-21 23:27:01 +0530

    Declaration and Definition of Funtion

---
+++ Python_Applications/Function1.py
@@ -0,0 +1,13 @@
+def main():
+    print("Enter First number: ")
+    value1 = int(input())
+
+    print("Enter Second number: ")
+    value2 = int(input())
+
+    Ret = value1 + value2
+
+    print("Addition is: ",Ret)
+
+if __name__ == "__main__":
+    main()
