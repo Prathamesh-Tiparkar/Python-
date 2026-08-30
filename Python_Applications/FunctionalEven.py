8c0b4f95c6bfcd91c99d98624712261bf6a8073b
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-08-30 17:44:07 +0530

    Add files via upload

---
+++ Python_Applications/FunctionalEven.py
@@ -0,0 +1,16 @@
+
+CheckEven = lambda No : (No % 2 == 0)
+
+def main():
+    Value = int(input("Enter number : "))
+
+    Ret = CheckEven(Value)          # Ret = (Value % 2 == 0)
+
+    if(Ret == True):
+        print("Its Even number")
+    else:
+        print("Its Odd Number")
+
+if __name__ == "__main__":
+    main()
+    
