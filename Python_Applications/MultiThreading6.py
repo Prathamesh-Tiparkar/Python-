854bfbd63144945618467ee8ce181d3797b2602b
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-05 00:06:13 +0530

    Add files via upload

---
+++ Python_Applications/MultiThreading6.py
@@ -0,0 +1,37 @@
+import time
+
+# 2+4+6+8 = 20
+def SumEven(No):
+    Sum = 0
+    
+    for i in range(2,No,2):
+        Sum = Sum + i
+
+    print("Summation of Even : ",Sum)
+
+# 1+3+5+7+9 = 25
+def SumOdd(No):
+    Sum = 0
+    
+    for i in range(1,No,2):
+        Sum = Sum + i
+
+    print("Summation of Odd : ",Sum)
+
+def main():
+
+    start_time = time.perf_counter()
+
+    SumEven(100000000)
+    SumOdd(100000000)
+
+    end_time = time.perf_counter()
+
+    print(f"Time required is : {end_time - start_time:.5f}")
+
+if __name__ == "__main__":
+    main()
+
+'''Summation of Even :  2499999950000000
+    Summation of Odd :  2500000000000000
+    Time required is : 8.61609  '''
