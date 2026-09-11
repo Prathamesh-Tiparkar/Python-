ac7616b330a630be9ee67d6299e6f03cb205836e
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-11 18:03:57 +0530

    Add files via upload

---
+++ Python_Applications/ExceptionDemo3.py
@@ -0,0 +1,25 @@
+def main():
+
+    Ans = 0
+    try:
+
+        print("Enter 1st number: ")
+        No1 = int(input())
+
+        print("Enter 2nd number: ")
+        No2 = int(input())   
+
+        Ans = No1/No2
+
+        print("Division is Sucessful")
+
+    except ZeroDivisionError as zobj:
+        print("Exception Occured due to second operand is zero",zobj)
+
+    except ValueError as vobj:
+        print("Exception Occured due to Invalid datatype",vobj)
+
+    print("Result is : ",Ans)
+
+if __name__ == "__main__":
+    main()
