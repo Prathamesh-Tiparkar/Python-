789354ba443902223c5c37e5eb758b0e1b528cc7
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-11 18:03:26 +0530

    Exception Demo

---
+++ Python_Applications/ExceptionDemo1.py
@@ -0,0 +1,14 @@
+def main():
+
+    print("Enter 1st number: ")
+    No1 = int(input())
+
+    print("Enter 2nd number: ")
+    No2 = int(input())   
+
+    Ans = No1/No2
+
+    print("Result is : ",Ans)
+
+if __name__ == "__main__":
+    main()
