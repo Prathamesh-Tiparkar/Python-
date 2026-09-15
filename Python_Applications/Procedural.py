b2ee1495b6b331d86affa91e7a86a2c4e5622e02
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-09-15 10:02:26 +0530

    Add files via upload

---
+++ Python_Applications/Procedural.py
@@ -0,0 +1,20 @@
+
+def Addition(No1, No2):
+    Ans = No1 + No2
+    return Ans
+
+def Substarction(No1, No2):
+    Ans = No1 - No2
+    return Ans
+
+print("Enter 1st number: ")
+Value1 = int(input())
+
+print("Enter 2nd number: ")
+Value2 = int(input())
+
+Ret = Addition(Value1, Value2)
+print("Additon is : ",Ret)
+
+Ret = Substarction(Value1, Value2)
+print("Substraction is : ",Ret)
---
+++ Python_Applications/Scripting.py
@@ -0,0 +1,14 @@
+
+print("Enter 1st number: ")
+No1 = int(input())
+
+print("Enter 2nd number: ")
+No2 = int(input())
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
+
+Ans = No1 - No2
+
+print("Substraction is : ",Ans)
